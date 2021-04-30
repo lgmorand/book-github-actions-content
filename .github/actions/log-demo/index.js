a5ba2f26060f56fd26a511470fd6725f725cfce0
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-04-30 21:34:40 +0200

    Update index.js

--- .github/actions/log-demo/index.js
+++ .github/actions/log-demo/index.js
@@ -2,13 +2,18 @@ const core = require('@actions/core');
 
 try {
   core.info('Here is some log');
-  
+  core.info('')
+  core.info('')
   core.warning('And a warning message');
-  
+  core.info('')
+  core.info('')
   core.error('Or an error one');
-  
+  core.info('')
+  core.info('')
   core.info('ℹ️ I can also embed icons if I want');
-  
+  core.info('any type of icon 💥');
+  core.info('')
+  core.info('')
   core.startGroup('A group of log lines')
   core.info('my first log')
   core.info('my second log')
