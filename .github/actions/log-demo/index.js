cafff41c8fc14128b99fb8965d176d62e91e97f2
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-04-30 21:36:30 +0200

    Update index.js

--- .github/actions/log-demo/index.js
+++ .github/actions/log-demo/index.js
@@ -10,6 +10,10 @@ try {
   core.error('Or an error one');
   core.info('')
   core.info('')
+  core.info('\u001b[38;5;6mI can play with colors')
+  core.info('\u001b[48;5;6mEven the background')
+  core.info('')
+  core.info('')
   core.info('ℹ️ I can also embed icons if I want');
   core.info('any type of icon 💥');
   core.info('')
